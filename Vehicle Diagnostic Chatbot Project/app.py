4d5fe440a52ba5938fe7002296aae49137508c44
Vishnumurali98 <vishnupp001@gmail.com>
2026-05-17 07:13:23 +0100

    Fixed Render port configuration

--- Vehicle Diagnostic Chatbot Project/app.py
+++ Vehicle Diagnostic Chatbot Project/app.py
@@ -364,5 +364,6 @@ def chatting():
 # RUN
 # ----------------------
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=5000)
 # -------end---------
